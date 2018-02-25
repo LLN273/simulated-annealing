806ea90f915d7977dd17281df2bcbe34fe65c7bd
LLN273 <luis.leal@ebc.uu.se>
2018-02-25 23:52:48 +0100

    corrected typo

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='travelfast',
       version='0.1',
       description=' Solves traveling salesperson problem using the simulated annealing optimization algorithm.',
       url='https://github.com/LLN273/simulated-annealing/travelfast',
-      author='Luis Leal',,,
+      author='Luis Leal',
       packages=['travelfast'],
       zip_safe=False)
+
